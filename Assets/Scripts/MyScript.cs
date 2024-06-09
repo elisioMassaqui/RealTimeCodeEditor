6e90ee85666c5c2be70c9324f748f8df5b259624
Mr.Robot != false <145590545+elisioMassaqui@users.noreply.github.com>
2024-06-09 12:07:26 +0100

    instaciar

--- Assets/Scripts/MyScript.cs
+++ Assets/Scripts/MyScript.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class MyScript : MonoBehaviour
 {
     public string myVariable = "Hello";
+    public GameObject bolinhaPrefab;
+    public Transform bolinhaPosition;
+
+    public void instanciar()
+    {
+        Instantiate(bolinhaPrefab, bolinhaPosition);
+    }
 
     // Outros métodos e lógica do script...
 
